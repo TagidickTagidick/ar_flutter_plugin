c459f01981aa14343d32424ef414b6c6979978bd
TagidickTagidick <tagidick.tagidick@gmail.com>
2023-10-08 07:50:56 +0300

    change to sceneform ux fragment

--- android/src/main/kotlin/io/carius/lars/ar_flutter_plugin/AndroidARView.kt
+++ android/src/main/kotlin/io/carius/lars/ar_flutter_plugin/AndroidARView.kt
@@ -499,7 +499,7 @@ internal class AndroidARView(
         if (argShowAnimatedGuide == true) { // explicit comparison necessary because of nullable type
             showAnimatedGuide = true
             val view = activity.findViewById(R.id.content) as ViewGroup
-            animatedGuide = activity.layoutInflater.inflate(com.google.ar.sceneform.ux.R.layout.sceneform_plane_discovery_layout, null)
+            animatedGuide = activity.layoutInflater.inflate(com.google.ar.sceneform.ux.R.layout.sceneform_ux_fragment_layout, null)
             view.addView(animatedGuide)
         }
 
