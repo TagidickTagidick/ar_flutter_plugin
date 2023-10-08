c84a7bf0dc8410e89b8d4290b9e17855f7e3d181
TagidickTagidick <tagidick.tagidick@gmail.com>
2023-10-08 08:35:53 +0300

    add custom layout

--- android/src/main/kotlin/io/carius/lars/ar_flutter_plugin/AndroidARView.kt
+++ android/src/main/kotlin/io/carius/lars/ar_flutter_plugin/AndroidARView.kt
@@ -32,8 +32,6 @@ import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.nio.FloatBuffer
 import java.util.concurrent.CompletableFuture
-
-import android.R
 import com.google.ar.sceneform.rendering.*
 
 import android.view.ViewGroup
@@ -499,7 +497,7 @@ internal class AndroidARView(
         if (argShowAnimatedGuide == true) { // explicit comparison necessary because of nullable type
             showAnimatedGuide = true
             val view = activity.findViewById(R.id.content) as ViewGroup
-            animatedGuide = activity.layoutInflater.inflate(com.google.ar.sceneform.ux.R.layout.sceneform_plane_discovery_layout, null)
+            animatedGuide = activity.layoutInflater.inflate(R.layout.custom_plane_discovery_layout, null)
             view.addView(animatedGuide)
         }
 
